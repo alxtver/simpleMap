b0edb79271e87320d16d46dc9a413a371e2c6efa
Kalinin <alxtver@gmail.com>
2023-02-20 22:39:30 +0300

    карта для проверки тайлов

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,71 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import Map from 'ol/Map.js';
+import OSM from 'ol/source/OSM.js';
+import TileLayer from 'ol/layer/Tile.js';
+import View from 'ol/View.js';
+import MousePosition from 'ol/control/MousePosition';
+import {createStringXY} from "ol/coordinate";
+import {defaults as defaultControls} from 'ol/control.js';
+import Graticule from 'ol/layer/Graticule';
+import Stroke from 'ol/style/Stroke';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const defaultUrl = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+const urlFromLocalStorage = localStorage.getItem('serverurl');
+const urlInput = document.querySelector('.server-url') as HTMLInputElement
+urlInput.value = urlFromLocalStorage ? urlFromLocalStorage : ''
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const url = urlInput.value === '' ? defaultUrl : `${urlInput.value}?&z={z}&x={x}&y={y}`;
+
+
+const mousePositionControl = new MousePosition({
+    coordinateFormat: createStringXY(4),
+    projection: 'EPSG:4326',
+    className: 'custom-mouse-position',
+    target: document.getElementById('mouse-position') as HTMLElement,
+});
+
+const tileLayer = new TileLayer({
+        source: new OSM({
+            url
+        }),
+    })
+tileLayer.set('id', 'osmId');
+
+const graticule = new Graticule({
+    // the style to use for the lines, optional.
+    strokeStyle: new Stroke({
+        color: 'rgba(84,41,3,0.5)',
+        width: 1,
+        lineDash: [3, 1],
+    }),
+    showLabels: true,
+    wrapX: false,
+})
+
+const map = new Map({
+    controls: defaultControls().extend([mousePositionControl]),
+    layers: [tileLayer, graticule],
+    target: 'map',
+    view: new View({
+        center: [0, 0], zoom: 2,
+    }),
+});
+
+const view = map.getView();
+view.on('change:center', () => {
+    const zoomValue = document.querySelector('.zoom-value');
+    if (zoomValue) {
+        zoomValue.innerHTML = view.getZoom()!.toFixed();
+    }
+})
+
+urlInput.addEventListener('input', (e: Event): void => {
+    const value = (e.target as HTMLInputElement).value;
+    localStorage.setItem('serverurl', value);
+
+    if (value) {
+        (tileLayer.getSource() as OSM).setUrl(value);
+    } else {
+        (tileLayer.getSource() as OSM).setUrl(defaultUrl);
+    }
+})
